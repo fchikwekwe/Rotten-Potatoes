fc290e5058362a6d4f83f3141e4253ddd2387a6e
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-09-26 19:28:14 -0700

    bug fix

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,26 +1,35 @@
 const MovieDb = require('moviedb-promise');
-const moviedb = new MovieDb('23361b10253ea4a933bb622178f943e7')
+const moviedb = new MovieDb('23361b10253ea4a933bb622178f943e7');
 const Review = require('../models/review.js');
 
 module.exports = (app) => {
       // ROOT
     app.get('/', (req, res) => {
-        moviedb.miscNowPlayingMovies().then(response => {
+        moviedb.miscNowPlayingMovies()
+        .then(response => {
             res.render('movies-index', { movies: response.results });
-        }).catch(console.error)
-    })
+        })
+        .catch(console.error)
+    });
 
-    // SHOW
+    // // SHOW
     app.get('/movies/:id', (req, res) => {
-        moviedb.movieInfo({ id: req.params.id }).then(movie =>
-            moviedb.movieVideos({ id: req.params.id }).then(videos => {
-                movie.trailer_youtube_id = videos.results[0].key
-                renderTemplate(movie);
-            })
-            function renderTemplate(movie) {
-                Review.find({ movieId: req.params.id }).then(reviews => {
-                    res.render('movies-show', { movie: movie, reviews: reviews });
-                })
-            })
-        }).catch(console.error)
+        moviedb.movieInfo({ id: req.params.id })
+        .then(movie => {
+            // if conditional <=================================
+             moviedb.movieVideos({ id: req.params.id })
+             .then(videos => {
+                 movie.trailer_youtube_id = videos.results[0].key
+                 renderTemplate(movie);
+             })
+                .catch(console.error)
+             function renderTemplate(movie) {
+                 Review.find({ movieId: req.params.id })
+                 .then(reviews => {
+                     res.render('movies-show', { movie: movie, reviews: reviews });
+                     })
+                }
+          })
+          .catch(console.error)
     })
+}
